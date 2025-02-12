9182dee772458bd4f360c0b5f4cd8db1736196a8
Freel11 <jadefreeljd@gmail.com>
2025-02-12 11:35:23 -0500

    Added base files for theme

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+
+<head>
+    <!-- Required meta tags -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="<?php bloginfo('charset'); ?>">
+
+    <?php wp_head(); ?>
+</head>
+
+<body>
+    <?php wp_body_open(); ?>
